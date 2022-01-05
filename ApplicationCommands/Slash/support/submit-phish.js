eca7d4eef79d129abb533b12d5159cf20e7ecb86
Sebi <sebi@thesebi.dev>
2022-01-06 00:46:02 +0100

    added blacklist for spamm reports

--- ApplicationCommands/Slash/support/submit-phish.js
+++ ApplicationCommands/Slash/support/submit-phish.js
@@ -29,48 +29,89 @@ module.exports = {
      * @param {String[]} args
      */
     
+
+    
     execute: async (client, interaction, args) => {
         
+        async function report(links = "")
+        {
+            if(!links || links == "")
+            {
+                const embed = new Discord.MessageEmbed()
+                    .setColor(colors.Blurple)
+                    .setTitle(`${client.emojis.cache.get(emojis.Bot_Cross).toString()} Link Submission canceled!`)
+                    .setDescription(`• The link(s) you provided are not valid!\n•be sure to paste the complete link!`)
+                    .setTimestamp()
+                return await interaction.reply({embeds: [embed]})
+            }
+            else
+            {
+                try
+                {
+                    let i = "";
+                    links.forEach(link => {
+                        i += link + "\n"
+                    });
+                    const embed = new Discord.MessageEmbed()
+                        .setColor(colors.Red)
+                        .setDescription(`${interaction.user.username} submitted: \n ${i}`)
+                        .setFooter(`Reporter Account ID: ${interaction.user.id}`)
+                        .setTimestamp()
+                    await channel.send({ content: `<@280094303429197844> New Submission!`, embeds: [embed]}).catch((err) => {})
+                    const embed1 = new Discord.MessageEmbed()
+                        .setColor(colors.Green)
+                        .setTitle(`${client.emojis.cache.get(emojis.IconMod).toString()} Link Submission successfull!`)
+                        .setDescription(`• Thanks for Submitting, if valid; all links provided will be added as malicious!`)
+                        .setTimestamp()
+                    return await interaction.reply({embeds: [embed1]})
+                }
+                catch(err)
+                {
+                    return await interaction.reply(`${client.emojis.cache.get(emojis.Bot_Emergency).toString()} An Error Accured!, please contact https://discord.gg/zbeg7vrkvN \n` + "ErrorCode: `skejfgikawesfbskjhfwoigf8234awdawdDaddy`")
+                }
+            }
+        }
+
         const link = await (interaction.options._hoistedOptions.find(o => o.name === "link").value.toString().toLowerCase())
         const regex = /(https?:\/\/[^\s]+)/g;
         const guild = await client.guilds.cache.find(g => g.id === "850690156582273054") // Bot Testing Server
         const channel = await guild.channels.cache.find(ch => ch.id === "928415970270249011") // Bot Submit Channel
         const links = link.match(regex)
-        if(!links)
-        {
-            const embed = new Discord.MessageEmbed()
-                .setColor(colors.Blurple)
-                .setTitle(`${client.emojis.cache.get(emojis.Bot_Cross).toString()} Link Submission canceled!`)
-                .setDescription(`• The link(s) you provided are not valid!\n•be sure to paste the complete link!`)
-                .setTimestamp()
-            return await interaction.reply({embeds: [embed]})
-        }
-        else
-        {
-            try
-            {
-                let i = "";
-                links.forEach(link => {
-                    if(link != "undefined") { i += link + "\n" }
-                });
 
-                const embed = new Discord.MessageEmbed()
-                    .setColor(colors.Red)
-                    .setDescription(`${interaction.user.username} submitted: \n ${i}`)
-                    .setTimestamp()
-                await channel.send({ content: `<@280094303429197844> New Submission!`, embeds: [embed]}).catch((err) => {})
-
-                const embed1 = new Discord.MessageEmbed()
-                    .setColor(colors.Green)
-                    .setTitle(`${client.emojis.cache.get(emojis.IconMod).toString()} Link Submission successfull!`)
-                    .setDescription(`• Thanks for Submitting, if valid; all links provided will be added as malicious!`)
-                    .setTimestamp()
-                return await interaction.reply({embeds: [embed1]})
-            }
-            catch(err)
+        con.query(
             {
-                return await interaction.reply(`${client.emojis.cache.get(emojis.Bot_Emergency).toString()} An Error Accured!, please contact https://discord.gg/zbeg7vrkvN \n` + "ErrorCode: `skejfgikawesfbskjhfwoigf8234awdawdDaddy`")
-            }
-        }
+              sql: `SELECT * FROM ${process.env.DB_DATABASENAME} WHERE id=?`,
+              timeout: 10000, // 10s
+              values: [interaction.user.id],
+            },
+            async function (err, result, fields) {
+                if (err) throw err;
+                if(Object.values(result).length == 0)
+                {
+                   
+                    con.query(
+                    {
+                        sql: `INSERT INTO ${process.env.DB_DATABASENAME} (id, username, discriminator) VALUES (?, ?, ?)`,
+                        timeout: 10000, // 10s
+                        values: [interaction.user.id, interaction.user.username, interaction.user.discriminator],
+                    },
+                    async function (err) {
+                        if (err) throw err;
+                        else
+                        {
+                            await report(links)
+                        }
+                    });
+
+                }
+                else if(result[0].report_blacklisted === "true")
+                {
+                    return await interaction.reply(`${client.emojis.cache.get(emojis.Bot_Emergency).toString()} An Error Accured!, You are Blacklisted from submitting reports \n` + ":warning: ErrorCode: `Account_Blacklisted`")
+                }
+                else
+                {
+                    await report(links)
+                }
+            })
     },
 };
