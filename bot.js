3fc951eee620ef2fce731f54eb8a0d2161a12ea9
Sebi <sebi@thesebi.dev>
2022-01-24 17:50:38 +0100

    added API for stats

--- bot.js
+++ bot.js
@@ -17,3 +17,28 @@ client.ArrayOfApplicationCommands = new Discord.Collection();
 require("./handler")(client);
 
 client.login(process.env.TOKEN);
+
+
+const express = require('express')
+const app = express()
+const port = 1000
+
+app.post('/stats', (req, res) => {
+  
+  const format = `de`
+  const servers = new Intl.NumberFormat(format).format(client.guilds.cache.size)
+  const users = new Intl.NumberFormat(format).format(client.guilds.cache.reduce((a, g) => a + g.memberCount, 0))
+  const averageUsers = new Intl.NumberFormat(format).format(((client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)) / client.guilds.cache.size).toFixed(0))
+  
+  const json = {
+    servers: servers,
+    users: users,
+    averageUsers: averageUsers,
+  }
+  
+  res.send(json)
+})
+
+app.listen(port, () => {
+  console.log(`API launched on port ${port}`)
+})
