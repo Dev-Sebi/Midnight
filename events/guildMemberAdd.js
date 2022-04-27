88fbecbc1b602179610e3254f652102965ba029f
Sebi <sebi@thesebi.dev>
2022-04-27 13:55:33 +0200

    added serverstats for website

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -41,7 +41,7 @@ client.on("guildMemberAdd", async (member) => {
                     else if(scammer === "true")
                     {
                         let action = ""
-                        const channel = await member.guild.channels.cache.find(ch => ch.id === result[0].logchannel)
+                        const channel = member.guild.channels.cache.find(ch => ch.id === result[0].logchannel)
                         const userdm = member
 
                         const badActorNoSetup = new Discord.MessageEmbed()
@@ -69,12 +69,12 @@ client.on("guildMemberAdd", async (member) => {
                         else if(result[0].action_scammer === "ban") { action = "ban" }
                         else { return await channel?.send({ embeds: [badActorNoSetup]}); }
 
+
                         if(action === "kick")
                         {
                             await member.kick({reason: 'Midnight Auto Moderation - Phish Link or Scammer Detected' }).then(channel?.send({ embeds: [badActorSetupKick] })).catch((err) => { 
                                 return channel?.send({ embeds: [noPermissions]}).catch((err) => {});
                             }).then(userdm.send(`You have been **kicked** from **${member.guild.name}** for being detected as a Scam account!`))
-                            return;
                         }
 
                         else if(action === "ban")
@@ -82,7 +82,37 @@ client.on("guildMemberAdd", async (member) => {
                             await member.ban({reason: 'Midnight Auto Moderation - Phish Link or Scammer Detected' }).then(channel?.send({ embeds: [badActorSetupBan] })).catch((err) => { 
                                 return channel?.send({ embeds: [noPermissions]}).catch((err) => {});
                             }).then(userdm.send(`You have been **banned** from **${member.guild.name}** for being detected as a Scam account!`))
-                            return;
+                        }
+
+                        if(action === "kick" || action === "ban")
+                        {
+                            con.query(
+                                {
+                                sql: `SELECT * FROM ${process.env.DB_DATABASEGUILDS} WHERE id = ?`,
+                                timeout: 10000, // 10s
+                                values: [member.guild.id],
+                                },
+                                async function (err, result, fields) {
+                                    if (err) throw err;
+                                    if (Object.values(result).length == 0)
+                                    {
+                                        return;
+                                    }            
+                                    const users_punished = (parseInt(result[0].users_punished) + 1).toString()
+                                    const users_prevented_from_joining = (parseInt(result[0].users_prevented_from_joining) + 1).toString()
+                                    
+                                    con.query(
+                                        {
+                                        sql: `UPDATE ${process.env.DB_DATABASEGUILDS} SET users_punished = ?, users_prevented_from_joining = ? WHERE id = ?`,
+                                        timeout: 10000, // 10s
+                                        values: [users_punished, users_prevented_from_joining, member.guild.id],
+                                        },
+                                        async function (err, result, fields) {
+                                            if (err) throw err;
+                                        }
+                                    )
+                                }
+                            )
                         }
                     }
                 }
